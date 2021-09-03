2345209d0ef56c5fa8efaf53f6eb97d35437383e
Manav Misra <manavm@visionwebsoft.com>
2021-09-02 21:18:57 -0500

    Find existing user

--- app/controller.js
+++ app/controller.js
@@ -4,7 +4,14 @@ import client from "./loader.js";
 const userConnection = client.db("user_db").collection("users");
 
 export default {
-  add(newUser) {
+  async add(newUser) {
+    // TODO: Find if the ✉️ already exists
+
+    /**
+     * 2. If it exists, return an error.
+     * 3. If it doesn't exist, create the user.
+     */
+    const existingUser = await userConnection.findOne({ email: newUser.email });
     return userConnection.insertOne(newUser);
   },
 };
