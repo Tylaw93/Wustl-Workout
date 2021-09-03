e35b7a2b5d1bf6935c21d3eac15a92c0d93a7baa
Manav Misra <manavm@visionwebsoft.com>
2021-09-02 21:25:09 -0500

    Mixin date informations

--- app/controller.js
+++ app/controller.js
@@ -5,13 +5,18 @@ const userConnection = client.db("user_db").collection("users");
 
 export default {
   async add(newUser) {
-    // TODO: Find if the ✉️ already exists
-
-    /**
-     * 2. If it exists, return an error.
-     * 3. If it doesn't exist, create the user.
-     */
     const existingUser = await userConnection.findOne({ email: newUser.email });
-    return userConnection.insertOne(newUser);
+
+    if (existingUser) {
+      throw new Error("User already exists");
+    }
+
+    const date = new Date();
+
+    return userConnection.insertOne({
+      ...newUser,
+      userCreated: date,
+      lastUpdated: date,
+    });
   },
 };
