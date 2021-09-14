d22a0227f51858665fe420f34bdc9ee2550cde3f
Tyler Lawson <tylaw93@yahoo.com>
2021-09-14 13:05:19 -0500

    Added routes

--- app/router.js
+++ app/router.js
@@ -1,24 +1,53 @@
 import Router from "express";
-import usersController from "./controller.js";
-import userModel from "./model.js";
+import workoutController from "./controller.js";
 
 const router = new Router();
 
-router.get("/", (_, res) => {
-  res.send("Hello ApI");
+router.get("/workouts", async (_, res) => {
+  try {
+    const workout = await workoutController.index();
+    res.json(workout);
+  } catch (error) {
+    res.status(400).send(error);
+  }
 });
 
-router.post("/", async (req, res) => {
+router.put("/workouts/:id", async (req, res) => {
   try {
-    const validatedUser = userModel.createUser(req.body);
-    const newUser = await usersController.add(validatedUser);
-    res.status(201).json(newUser);
+    const updatedWorkout = await workoutController.update(
+      req.params.id,
+      req.body
+    );
+    res.status(200).send(updatedWorkout);
   } catch (error) {
-    if (error.message.startsWith("User")) {
-      res.status(400).send(error.message);
-    } else {
-      res.status(500).send(error.message);
-    }
+    res.status(404).json(error.message);
+  }
+});
+
+router.post("/workouts", async (_, res) => {
+  try {
+    const newWorkout = await workoutController.create();
+    res.status(201).json(newWorkout);
+  } catch (error) {
+    res.status(400).json(error.message);
+  }
+});
+
+router.get("/workouts/range", async (_, res) => {
+  try {
+    const workouts = await workoutController.show();
+    res.json(workouts);
+  } catch (err) {
+    res.status(500).json({ message: err.message });
+  }
+});
+
+router.delete("/workouts/", async (req, res) => {
+  try {
+    await workoutController.deleteAll();
+    res.status(204).send();
+  } catch (error) {
+    res.status(404).json(error.message);
   }
 });
 
